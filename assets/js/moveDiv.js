79fce97870a7fbc57a8be9e67c83a1a141684c39
Andrey Korolev <44708327+PoppieHub@users.noreply.github.com>
2022-01-09 03:44:03 +0500

    Update moveDiv.js

--- assets/js/moveDiv.js
+++ assets/js/moveDiv.js
@@ -3,8 +3,8 @@ let div = document.querySelector('.card');
 div.onmousedown = function(e) {
 
     let coords = getCoords(div);
-    let shiftX = e.pageX - coords.left;
-    let shiftY = e.pageY - coords.top;
+    let shiftX = e.pageX  - 50;
+    let shiftY = e.pageY - 50;
 
 
     //document.body.appendChild(div);
@@ -12,7 +12,7 @@ div.onmousedown = function(e) {
 
     function moveAt(e) {
         div.style.left = e.pageX - shiftX + 'px';
-        div.style.top = e.pageY + 192 - shiftY + 'px';
+        div.style.top = e.pageY - shiftY + 'px';
     }
 
     document.onmousemove = function(e) {
@@ -26,7 +26,7 @@ div.onmousedown = function(e) {
 
 }
 
-function getCoords(elem) { 
+function getCoords(elem) {   // кроме IE8-
     let box = elem.getBoundingClientRect();
     return {
         top: box.top + pageYOffset,
